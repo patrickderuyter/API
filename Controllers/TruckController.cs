8b2f1a5930c6962fdcb612c892365a127a4fb25d
Patrick de Ruijter <patrick.de.ruijter@ict.nl>
2023-02-23 14:37:34 +0100

    Trucksettings werkend

---
+++ Controllers/TruckController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LESAPI.Controllers
+{
+    using TruckWebService;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TruckController : ControllerBase
+    {
+        private readonly ILogger<TruckController> _logger;
+
+        public TruckController(ILogger<TruckController> logger)
+        {
+            _logger = logger;
+
+        }
+        [HttpGet("GeefTruckConfiguratie/{trucknummer}")]
+        public async Task<TruckSettings> GeefTruckConfiguratie(string trucknummer)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.GeefTruckConfiguratieAsync(trucknummer);
+            return result;
+        }
+
+        [HttpPut("OpslaanTruckConfiguratie")]
+        public async Task OpslaanTruckConfiguratie(TruckSettings truckSettings)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            await serviceClient.OpslaanTruckConfiguratieAsync(truckSettings);
+        }
+    }
+}
