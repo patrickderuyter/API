d9e426e523ec1a23294ba7e3389c23c0c0981c8c
Patrick de Ruijter <patrick.de.ruijter@ict.nl>
2023-02-24 09:21:37 +0100

    no message

--- Controllers/OpdrachtenController.cs
+++ Controllers/OpdrachtenController.cs
@@ -2,6 +2,7 @@
 
 namespace LESAPI.Controllers
 {
+    using LESAPI.Models;
     using TruckWebService;
 
     [ApiController]
@@ -28,5 +29,41 @@ namespace LESAPI.Controllers
 
 
         }
+
+        [HttpGet("GeefLocatieInfoVolgendeLocatie/{huidigelocatie}")]
+        public async Task<LocatieInfo> GeefLocatieInfoVolgendeLocatie(string huidigelocatie)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.GeefLocatieInfoVolgendeLocatieAsync(huidigelocatie);
+
+            return result;
+        }
+
+
+        [HttpGet("GeefLocatieInfo/{locatienummer}")]
+        public async Task<LocatieInfo> GeefLocatieInfo(string locatienummer)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.GeefLocatieInfoAsync(locatienummer);
+            return result;
+        }
+
+        [HttpPost("VerwerkTelOpdracht")]
+        public async Task<bool> VerwerkTelOpdracht(LocatieInfoWithPin locatieInfoWithPin)
+        {
+            try
+            {
+                await using var serviceClient = new TruckWebServiceClient();
+                await serviceClient.VerwerkTelOpdrachtAsync(locatieInfoWithPin.LocatieInfo, locatieInfoWithPin.Pin);
+            }
+            catch
+            {
+                return false;
+
+            }
+
+            return true;
+        }
+
     }
 }
---
+++ Models/LocatieInfoWithPin.cs
@@ -0,0 +1,10 @@
+﻿namespace LESAPI.Models
+{
+    using TruckWebService;
+
+    public class LocatieInfoWithPin
+    {
+        public LocatieInfo LocatieInfo { get; set; }
+        public string Pin { get; set; }
+    }
+}
