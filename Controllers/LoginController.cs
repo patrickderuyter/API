191f56eab20e422af5260cb23cfc1a2793092828
Patrick de Ruijter <patrick.de.ruijter@ict.nl>
2023-03-18 20:32:08 +0100

    Update

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LESAPI.Controllers
+{
+    using TruckWebService;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LastdragerController> _logger;
+
+        public LoginController(ILogger<LastdragerController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        
+        [HttpGet("Inloggen/{truckNumber}/{pin}")]
+        public async Task<LoginResultaat> Inloggen(string truckNumber, string pin)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.InloggenAsync(truckNumber, pin);
+            return result;
+        }
+
+
+        [HttpGet("GeefTruckConfiguratie/{truck}")]
+        public async Task<TruckSettings> GeefTruckConfiguratie(string truck)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.GeefTruckConfiguratieAsync(truck);
+            return result;
+        }
+    }
+}
---
+++ Controllers/VerzendingenController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LESAPI.Controllers
+{
+    using TruckWebService;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class VerzendingenController : ControllerBase
+    {
+        private readonly ILogger<VerzendingenController> _logger;
+
+        public VerzendingenController(ILogger<VerzendingenController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        
+        [HttpGet("GeefGmagEindcontrolePallets")]
+        public async Task<PalletEindcontrole[]> GeefGmagEindcontrolePallets()
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.GeefGmagEindcontrolePalletsAsync();
+            return result.ResultaatObject;
+        }
+
+        [HttpGet("GeefFabriekslocatiesBijEindcontrolePallets")]
+        public async Task<TruckWebService.Fabriekslocatie[]> GeefFabriekslocatiesBijEindcontrolePallets()
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.GeefFabriekslocatiesBijEindcontrolePalletsAsync();
+            return result;
+        }
+
+        [HttpPost("AnnuleerEindcontrole/{pallet}/{pincode}")]
+        public async Task<Resultaat> AnnuleerEindcontrole(string pallet,string pincode)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.AnnuleerEindcontroleAsync(pallet, pincode);
+            return result;
+        }
+
+        [HttpPost("AfrondenEindcontrolePalletnummmer/{pallet}/{pincode}")]
+        public async Task<Resultaat> AfrondenEindcontrolePalletnummmer(string pallet, string pincode)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.AfrondenEindcontrolePalletnummmerAsync(pallet, pincode);
+            return result;
+        }
+
+        [HttpGet("StartEindcontrole/{pallet}/{pincode}")]
+        public async Task<EindcontroleRegel[]> StartEindcontrole(string pallet, string pincode)
+        {
+            await using var serviceClient = new TruckWebServiceClient();
+            var result = await serviceClient.StartEindcontroleAsync(pallet, pincode);
+            return result.ResultaatObject;
+        }
+    }
+}
